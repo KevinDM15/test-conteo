388f562d0dbd670be2410a6011aaf1052abfca60
kevindm14 <kevin.diaz@therocketcode.com>
2025-05-02 21:37:47 -0500

    feat: :art: improve design

--- src/views/products/components/Products.tsx
+++ src/views/products/components/Products.tsx
@@ -80,105 +80,109 @@ export const Products = () => {
           <SideBarFilter />
 
           {/* Lista de productos */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-5">
+          <div>
             {/* Loading */}
             {isLoading ? (
-              <div className="flex justify-center items-center w-full h-64">
-                <svg
-                  className="animate-spin h-10 w-10 text-deep-900"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <circle
-                    className="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    strokeWidth="4"
-                  />
-                  <path
-                    className="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8v8H4z"
-                  />
-                </svg>
+              <div className="flex justify-center items-center">
+                <div className="flex justify-center items-center w-full h-64">
+                  <svg
+                    className="animate-spin h-10 w-10 text-deep-900"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    />
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8v8H4z"
+                    />
+                  </svg>
+                </div>
               </div>
             ) : filteredProducts.length > 0 ? (
-              filteredProducts.map((product, index) => (
-                <div
-                  key={index}
-                  className="bg-white rounded-xl shadow-md p-5 w-full hover:shadow-lg transition flex flex-col"
-                >
-                  <div className="flex flex-col">
-                    <span className="text-sm font-extrabold">
-                      Codigo{" "}
-                      <span className="text-deep-600">{product.code}</span>
-                    </span>
-                  </div>
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-5">
+                {filteredProducts.map((product, index) => (
+                  <div
+                    key={index}
+                    className="bg-white rounded-xl shadow-md p-5 w-full hover:shadow-lg transition flex flex-col"
+                  >
+                    <div className="flex flex-col">
+                      <span className="text-sm font-extrabold">
+                        Codigo{" "}
+                        <span className="text-deep-600">{product.code}</span>
+                      </span>
+                    </div>
 
-                  <div className="flex items-center justify-between mb-2">
-                    <h3 className="text-deep-900 font-bold text-lg">
-                      <div className="flex items-center">
-                        {product.name}{" "}
-                        <span className="ml-2 text-sm bg-gray-100 px-2 py-1 rounded-lg text-gray-500">
-                          {product.quantity}
-                        </span>
+                    <div className="flex items-center justify-between mb-2">
+                      <h3 className="text-deep-900 font-bold text-lg">
+                        <div className="flex items-center">
+                          {product.name}{" "}
+                          <span className="ml-2 text-sm bg-gray-100 px-2 py-1 rounded-lg text-gray-500">
+                            {product.quantity}
+                          </span>
+                        </div>
+                      </h3>
+
+                      <div className="flex space-x-2">
+                        <button
+                          onClick={() => handleEdit(product)}
+                          className="p-2 bg-deep-100 hover:bg-deep-200 rounded-lg transition-colors duration-200 cursor-pointer"
+                          aria-label="Editar producto"
+                        >
+                          <svg
+                            className="w-5 h-5 text-deep-900"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth={2}
+                              d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+                            />
+                          </svg>
+                        </button>
+                        <button
+                          onClick={() => handleDelete(product)}
+                          className="p-2 bg-red-100 hover:bg-red-200 rounded-lg transition-colors duration-200 cursor-pointer"
+                          aria-label="Eliminar producto"
+                        >
+                          <svg
+                            className="w-5 h-5 text-red-600"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth={2}
+                              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                            />
+                          </svg>
+                        </button>
                       </div>
-                    </h3>
+                    </div>
 
-                    <div className="flex space-x-2">
-                      <button
-                        onClick={() => handleEdit(product)}
-                        className="p-2 bg-deep-100 hover:bg-deep-200 rounded-lg transition-colors duration-200 cursor-pointer"
-                        aria-label="Editar producto"
-                      >
-                        <svg
-                          className="w-5 h-5 text-deep-900"
-                          fill="none"
-                          stroke="currentColor"
-                          viewBox="0 0 24 24"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-                          />
-                        </svg>
-                      </button>
-                      <button
-                        onClick={() => handleDelete(product)}
-                        className="p-2 bg-red-100 hover:bg-red-200 rounded-lg transition-colors duration-200 cursor-pointer"
-                        aria-label="Eliminar producto"
-                      >
-                        <svg
-                          className="w-5 h-5 text-red-600"
-                          fill="none"
-                          stroke="currentColor"
-                          viewBox="0 0 24 24"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                          />
-                        </svg>
-                      </button>
+                    <div className="text-sm text-deep-600 ">
+                      <p className="text-gray-400">{product.description}</p>
                     </div>
                   </div>
-
-                  <div className="text-sm text-deep-600 ">
-                    <p className="text-gray-400">{product.description}</p>
-                  </div>
-                </div>
-              ))
+                ))}
+              </div>
             ) : (
-              <div className="flex w-full bg-white justify-center items-center h-64 rounded-xl shadow-md p-5">
+              <div className="flex bg-white justify-center items-center h-64 rounded-xl shadow-md p-5">
                 <p className="text-gray-400 font-semibold">
                   Opps, no se encontraron productos.
                 </p>
